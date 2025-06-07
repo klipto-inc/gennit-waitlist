7ae726b2d777352e6525fa0cd0697afbb4327541
templeajuzie <kliptolite@gmail.com>
2025-06-07 16:57:45 +0100

    feat: update waitlist

--- app/comingsoon/page.tsx
+++ app/comingsoon/page.tsx
@@ -206,12 +206,12 @@ export default function ComingSoonPage() {
               className="pl-14 pr-36 bg-white/[0.03] border border-white/10 text-white placeholder-gray-500 focus-none h-16 text-base backdrop-blur-sm transition-all duration-300 rounded-2xl"
             />
             <Button
-              className="absolute right-3 top-3 bottom-3 bg-white text-black hover:bg-gray-100 px-2 md:px-8 text-base font-medium transition-all duration-300 rounded-xl flex flex-row items-center justify-center"
+              className="absolute right-3 top-3 bottom-3 bg-white text-black hover:bg-gray-100 px-2 md:px-8 text-base font-medium transition-all duration-300 rounded-xl flex flex-row gap-x-2 items-center justify-center"
               type="submit"
               disabled={loading}
             >
               <span className="hidden md:flex">Join</span>
-              <ArrowRight className="ml-3 h-5 w-5" />
+              <ArrowRight className="h-5 w-5" />
             </Button>
           </div>
 
